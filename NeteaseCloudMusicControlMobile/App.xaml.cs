3a0036c8d74af93d0b6af196bcfa1ec1cb2a0b4e
Miaoywww <375629202@qq.com>
2022-08-14 17:57:47 +0800

    添加项目文件。

---
+++ NeteaseCloudMusicControlMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NeteaseCloudMusicControlMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NeteaseCloudMusicControlMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NeteaseCloudMusicControlMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
