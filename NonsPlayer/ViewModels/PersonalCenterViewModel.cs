dc8a44d0818dabe57e62bb382dd0be7811e78c32
Miaoywww <375629202@qq.com>
2023-08-04 19:14:15 +0800

    Add: 添加问候语

--- NonsPlayer/ViewModels/PersonalCenterViewModel.cs
+++ NonsPlayer/ViewModels/PersonalCenterViewModel.cs
@@ -1,6 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml;
 using NonsPlayer.Contracts.Services;
+using NonsPlayer.Contracts.ViewModels;
 using NonsPlayer.Core;
 using NonsPlayer.Core.Account;
 using NonsPlayer.Core.Api;
@@ -9,13 +11,32 @@ using NonsPlayer.Models;
 
 namespace NonsPlayer.ViewModels;
 
-public class PersonalCenterViewModel : ObservableRecipient
+public class PersonalCenterViewModel : ObservableRecipient, INotifyPropertyChanged
 {
     public AccountState AccountState => AccountState.Instance;
-    public INavigationService NavigationService
+
+    public string Greeting
     {
-        get;
+        get
+        {
+            var hour = DateTime.Now.Hour;
+            switch (hour)
+            {
+                case int n when n >= 0 && n < 6:
+                    return "凌晨好!";
+                case int n when n >= 6 && n < 12:
+                    return "早上好!";
+                case int n when n >= 12 && n < 14:
+                    return "中午好!";
+                case int n when n >= 14 && n < 18:
+                    return "下午好!";
+                default:
+                    return "晚上好!";
+            }
+        }
     }
+
+    public INavigationService NavigationService { get; }
 
     public PersonalCenterViewModel(INavigationService navigationService)
     {
