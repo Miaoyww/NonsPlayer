95886d7a78c3d5a39c02190158605dc377685edd
Miaoywww <375629202@qq.com>
2023-08-08 17:25:23 +0800

    Change: 为设置做准备

--- NonsPlayer/Models/LocalSettingsOptions.cs
+++ NonsPlayer/Models/LocalSettingsOptions.cs
@@ -5,4 +5,10 @@ public class LocalSettingsOptions
     public string? ApplicationDataFolder { get; set; }
 
     public string? LocalSettingsFile { get; set; }
+    
+    /// <summary>
+    /// 播放歌单内音乐直接将歌曲添加至播放列表中
+    /// </summary>
+    public bool IsPlay2List { get; set; }
+
 }
