e67a947f51793415dc79edbbe37512e6e46542b4
Miaoywww <375629202@qq.com>
2023-08-09 20:23:27 +0800

    Add: 添加设置项

--- NonsPlayer/Models/LocalSettingsOptions.cs
+++ NonsPlayer/Models/LocalSettingsOptions.cs
@@ -5,10 +5,12 @@ public class LocalSettingsOptions
     public string? ApplicationDataFolder { get; set; }
 
     public string? LocalSettingsFile { get; set; }
-    
+
     /// <summary>
     /// 播放歌单内音乐直接将歌曲添加至播放列表中
     /// </summary>
     public bool IsPlay2List { get; set; }
 
+    public int RemotePort { get; set; }
+    
 }
