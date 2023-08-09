9680dd0f3127ed7003250aadd728d172a13f3cd9
Miaoywww <375629202@qq.com>
2023-08-10 00:22:38 +0800

    Add: 为正在播放的音乐添加提示

--- NonsPlayer/Components/ViewModels/PlayQueueItemCardViewModel.cs
+++ NonsPlayer/Components/ViewModels/PlayQueueItemCardViewModel.cs
@@ -1,6 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Windows.UI.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using NonsPlayer.Core.Models;
+using NonsPlayer.Core.Player;
 using NonsPlayer.Helpers;
 
 namespace NonsPlayer.Components.ViewModels;
--- NonsPlayer/Components/Views/PlayQueueItemCard.xaml.cs
+++ NonsPlayer/Components/Views/PlayQueueItemCard.xaml.cs
@@ -1,17 +1,24 @@
 ﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
 using NonsPlayer.Components.ViewModels;
 using NonsPlayer.Core.Models;
 using NonsPlayer.Core.Player;
 
 namespace NonsPlayer.Components.Views;
 
-public sealed partial class PlayQueueItemCard : UserControl, INotifyPropertyChanged
-{
+[INotifyPropertyChanged]
+public sealed partial class PlayQueueItemCard : UserControl
+{    
+    [ObservableProperty] private Brush fontBrush = (Brush) Application.Current.Resources["TextFillColorPrimaryBrush"];
+
     public PlayQueueItemCard()
     {
         ViewModel = App.GetService<PlayQueueItemCardViewModel>();
+        PlayQueue.Instance.CurrentMusicChanged += OnCurrentMusicChanged;
         InitializeComponent();
     }
 
@@ -22,13 +29,26 @@ public sealed partial class PlayQueueItemCard : UserControl, INotifyPropertyChan
         set => ViewModel.Music = value;
     }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    private void OnPropertyChanged(string propertyName)
+    public bool IsPlaying
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        set{
+            if (value)
+            {
+                FontBrush = (Brush) Application.Current.Resources["AccentFillColorSecondaryBrush"];
+            }
+        }
     }
-
+    private void OnCurrentMusicChanged(Music value)
+    {
+        if (value == ViewModel.Music)
+        {
+            FontBrush = (Brush) Application.Current.Resources["AccentFillColorSecondaryBrush"];
+        }
+        else
+        {
+            FontBrush = (Brush) Application.Current.Resources["TextFillColorPrimaryBrush"];
+        }
+    }
     public void Play(object sender, PointerRoutedEventArgs e)
     {
         PlayQueue.Instance.Play(ViewModel.Music);
