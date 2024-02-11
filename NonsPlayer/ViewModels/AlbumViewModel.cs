db4a5cc806888ebad46d465f386d1a557e7daae6
Miaoyww <miaomiaoywww@gmail.com>
2024-02-11 17:20:40 +0800

    Append: 忘记加新页面了

---
+++ NonsPlayer/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NonsPlayer.Contracts.ViewModels;
+using NonsPlayer.Core.Helpers;
+using NonsPlayer.Core.Models;
+using NonsPlayer.Core.Services;
+using NonsPlayer.Helpers;
+using System.Diagnostics;
+
+namespace NonsPlayer.ViewModels;
+
+public partial class AlbumViewModel : ObservableRecipient, INavigationAware
+{
+
+    [ObservableProperty] public Album currentAlbum;
+
+    public async void OnNavigatedTo(object parameter)
+    {
+        CurrentAlbum = (Album)parameter;
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+}
---
+++ NonsPlayer/Views/Pages/AlbumPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using NonsPlayer.ViewModels;
+
+namespace NonsPlayer.Views.Pages;
+
+public sealed partial class AlbumPage : Page
+{
+    public AlbumViewModel ViewModel
+    {
+        get;
+    }
+
+    public AlbumPage()
+    {
+        ViewModel = App.GetService<AlbumViewModel>();
+        InitializeComponent();
+    }
+}
