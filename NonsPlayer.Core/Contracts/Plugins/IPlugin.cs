92d3390b86ff0de98a16cbd278f01897b94bf4dd
Miaoyww <miaomiaoywww@gmail.com>
2024-06-25 21:52:47 +0800

    增加插件管理器

---
+++ NonsPlayer.Core/Contracts/Plugins/IPlugin.cs
@@ -0,0 +1,25 @@
+﻿namespace NonsPlayer.Core.Contracts.Plugins;
+
+public interface IPlugin
+{
+    PluginMetadata GetMetadata();
+    void Initialize();
+    void Uninitialize();
+}
+
+public class PluginMetadata
+{
+    public PluginMetadata This { get; set; }
+
+    public PluginMetadata()
+    {
+        This = this;
+    }
+
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public Version Version { get; set; }
+    public TimeSpan UpdateTime { get; set; }
+    public Uri Repository { get; set; }
+}
--- NonsPlayer.Core/Services/PluginService.cs
+++ NonsPlayer.Core/Services/PluginService.cs
@@ -1,8 +1,26 @@
-﻿using NonsPlayer.Core.Models;
+﻿using System.Reflection;
+using NonsPlayer.Core.Contracts.Plugins;
+using NonsPlayer.Core.Models;
 
 namespace NonsPlayer.Core.Services;
 
 public class PluginService
 {
-    public Dictionary<Music, int> Data;
+    private Dictionary<string, IPlugin> _plugins = new();
+    public static PluginService Instance { get; } = new();
+
+    public void LoadPlugins(string directory)
+    {
+       
+    }
+
+    public Tuple<string, IPlugin>? LoadSingleAdapter(string file)
+    {
+        return null;
+    }
+
+    public IPlugin GetAdapter(string platformName)
+    {
+        return null;
+    }
 }
