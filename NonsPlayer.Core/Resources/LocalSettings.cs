97b9c014358d53c344f55cb84c0a3a61f48da8df
Miaoyww <miaomiaoywww@gmail.com>
2024-06-29 23:20:44 +0800

    重构配置管理器，更直观易修改

---
+++ NonsPlayer.Core/Resources/LocalSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace NonsPlayer.Core.Resources
+{
+    public class LocalSettings
+    {
+        [JsonIgnore] public string DataPath { get; set; }
+        [JsonIgnore] public string ConfigFilePath;
+        [JsonPropertyName("adapter_path")] public string AdapterPath { get; set; }
+        [JsonPropertyName("plugin_path")] public string PluginPath { get; set; }
+        [JsonPropertyName("data_path")] public string Data { get; set; }
+        [JsonPropertyName("theme")] public string Theme { get; set; }
+
+        [JsonPropertyName("disabled_adapters")]
+        public string DisabledAdapters { get; set; }
+
+        [JsonPropertyName("disabled_plugins")] public string DisabledPlugins { get; set; }
+
+        public LocalSettings()
+        {
+            DataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/NonsPlayer";
+            ConfigFilePath = DataPath + "/config.json";
+            AdapterPath = DataPath + "/Adapters";
+            PluginPath = DataPath + "/Plugins";
+            Data = DataPath + "/Data";
+            Theme = "Light";
+            DisabledAdapters = "";
+            DisabledPlugins = "";
+        }
+    }
+}
--- NonsPlayer.Core/Services/ConfigManager.cs
+++ NonsPlayer.Core/Services/ConfigManager.cs
@@ -1,120 +1,57 @@
 ﻿using System.IO;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+using NonsPlayer.Core.Resources;
 
 namespace NonsPlayer.Core.Services;
 
-public class Config
-{
-    [JsonPropertyName("key")] public string Key { get; set; }
-
-    [JsonPropertyName("value")] private string Value { get; set; }
-
-    [JsonPropertyName("description")] public string Description { get; set; }
-
-
-    public Config(string key, string value, string description = "")
-    {
-        Key = key;
-        Value = value;
-        Description = description;
-    }
-
-    public void Set(string value)
-    {
-        Value = value;
-    }
-
-    public string Get()
-    {
-        return Value;
-    }
-}
-
 public class ConfigManager
 {
-    private readonly string _dataPath;
-
-
-    private readonly string _configFilePath;
-
-    private Dictionary<string, Config> _config;
-
     public static ConfigManager Instance { get; } = new();
+    public LocalSettings Settings;
 
     public ConfigManager()
     {
-        _dataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/NonsPlayer";
-        _configFilePath = _dataPath + "/config.json";
+        Settings = new();
     }
-
-    public void LoadConfigAsync()
+    public void LoadConfig()
     {
-        if (File.Exists(_configFilePath))
+        if (File.Exists(Settings.ConfigFilePath))
         {
             try
             {
-                string json = File.ReadAllText(_configFilePath);
-                _config = JsonSerializer.Deserialize<Dictionary<string, Config>>(json);
+                var json = File.ReadAllText(Settings.ConfigFilePath);
+                Settings = JsonSerializer.Deserialize<LocalSettings>(json);
             }
             catch
             {
-                _config = DefaultConfig();
+                // ignore
             }
         }
         else
         {
-            if (!File.Exists(_configFilePath))
+            if (!File.Exists(Settings.ConfigFilePath))
             {
-                Directory.CreateDirectory(Path.GetDirectoryName(_configFilePath));
-                File.Create(_configFilePath).Close();
+                Directory.CreateDirectory(Path.GetDirectoryName(Settings.ConfigFilePath));
+                File.Create(Settings.ConfigFilePath).Close();
             }
-
-            _config = DefaultConfig();
+            // ignore if not exists
         }
-    }
-
-    public Config GetConfig(string key)
-    {
-
-        return _config[key];
     }
 
     public void SaveConfig()
     {
-        if (!File.Exists(_configFilePath))
+        if (!File.Exists(Settings.ConfigFilePath))
         {
-            Directory.CreateDirectory(Path.GetDirectoryName(_configFilePath));
-            File.Create(_configFilePath).Close();
+            Directory.CreateDirectory(Path.GetDirectoryName(Settings.ConfigFilePath));
+            File.Create(Settings.ConfigFilePath).Close();
         }
 
         var options = new JsonSerializerOptions
         {
             Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
         };
-        string json = JsonSerializer.Serialize(_config, options);
-        File.WriteAllText(_configFilePath, json, Encoding.UTF8);
-    }
-
-    public Dictionary<string, Config> DefaultConfig()
-    {
-        Dictionary<string, Config> config = new();
-        List<Config> configs = new()
-        {
-            new("adapterPath", _dataPath + "/adapters", "适配器路径"),
-            new("pluginPath", _dataPath + "/plugins", "插件路径"),
-            new("data", _dataPath + "/data", "数据路径"),
-            new("theme", "Light", "主题"),
-            new("disabledAdapters", "", "禁用的适配器"),
-            new("disabledPlugins", "", "禁用的插件"),
-        };
-        foreach (var item in configs)
-        {
-            config[item.Key] = item;
-        }
-
-        return config;
+        var json = JsonSerializer.Serialize(Settings, options);
+        File.WriteAllText(Settings.ConfigFilePath, json, Encoding.UTF8);
     }
 }
