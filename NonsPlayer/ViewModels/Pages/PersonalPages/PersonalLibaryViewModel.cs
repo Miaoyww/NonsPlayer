a79ef84548ce431c6965cb95deeae668abd83761
Miaoyww <miaomiaoywww@gmail.com>
2024-07-27 16:28:38 +0800

    移动个人电台至我的页面

--- NonsPlayer/ViewModels/Pages/PersonalPages/PersonalLibaryViewModel.cs
+++ NonsPlayer/ViewModels/Pages/PersonalPages/PersonalLibaryViewModel.cs
@@ -2,12 +2,14 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
+using NonsPlayer.Components.Models;
 using NonsPlayer.Contracts.ViewModels;
 using NonsPlayer.Core.Contracts.Adapters;
 using NonsPlayer.Core.Contracts.Models.Music;
 using NonsPlayer.Core.Contracts.Models.Nons;
 using NonsPlayer.Core.Nons.Player;
 using NonsPlayer.Helpers;
+using System.Collections.ObjectModel;
 
 namespace NonsPlayer.ViewModels;
 
@@ -17,12 +19,13 @@ public partial class PersonalLibaryViewModel : ObservableRecipient, INavigationA
     [ObservableProperty] private int favoriteCount;
     [ObservableProperty] private string userName;
     [ObservableProperty] private ImageBrush avatar;
+    public ObservableCollection<MusicModel> TopSongs = new();
     private IAccount account;
-    private IAdapter adapter;
+    public IAdapter Adapter;
 
     public async void OnNavigatedTo(object parameter)
     {
-        adapter = parameter as IAdapter;
+        Adapter = parameter as IAdapter;
         await Task.Run(Init);
     }
 
@@ -32,37 +35,42 @@ public partial class PersonalLibaryViewModel : ObservableRecipient, INavigationA
 
     private async Task Init()
     {
-        account = adapter.Account.GetAccount();
-        var avatar = await account.GetAvatarUrlAsync();
+        account = Adapter.Account.GetAccount();
         if (account.IsLoggedIn)
         {
-            if (adapter.Account.FavoritePlaylist == null)
-            {
-                await adapter.Account.GetFavoritePlaylist();
-            }
-
+            var avatar = await account.GetAvatarUrlAsync();
             ServiceHelper.DispatcherQueue.TryEnqueue(() =>
             {
-                Avatar = new ImageBrush{ImageSource = new BitmapImage(new Uri(avatar))};
-                if (adapter.Account.FavoritePlaylist != null)
+                Avatar = new ImageBrush { ImageSource = new BitmapImage(new Uri(avatar)) };
+                UserName = account.Name;
+            });
+
+            await Adapter.Account.GetFavoritePlaylist();
+            FavoriteSongs = Adapter.Account.FavoritePlaylist;
+
+            if (Adapter.Account.FavoritePlaylist == null) return;
+
+            Task.WaitAll(FavoriteSongs.InitializeTracks(), FavoriteSongs.InitializeMusics());
+            var songs = Adapter.Account.FavoritePlaylist.Musics.Take(20)
+                .Select(music => new MusicModel { Music = music });
+            ServiceHelper.DispatcherQueue.TryEnqueue(() =>
+            {
+                foreach (MusicModel item in songs)
                 {
-                    FavoriteSongs = adapter.Account.FavoritePlaylist;
+                    TopSongs.Add(item);
+                }
+                if (Adapter.Account.FavoritePlaylist != null)
+                {
                     FavoriteCount = FavoriteSongs.MusicsCount;
-                    UserName = account.Name;
                 }
             });
         }
     }
 
     [RelayCommand]
-    public async Task PlayFavorite()
+    public void PlayFavorite()
     {
         PlayQueue.Instance.Clear();
-        if (FavoriteSongs.Musics == null)
-        {
-            await FavoriteSongs.InitializeTracks();
-            await FavoriteSongs.InitializeMusics();
-        }
         PlayQueue.Instance.AddMusicList(FavoriteSongs.Musics.ToArray());
         PlayQueue.Instance.PlayNext();
     }
