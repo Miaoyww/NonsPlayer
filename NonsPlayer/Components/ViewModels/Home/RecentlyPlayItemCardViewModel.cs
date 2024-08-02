0d8ce61e19016c63209c001d5fbbe65e013e2d0a
Miaoyww <miaomiaoywww@gmail.com>
2024-08-02 15:36:42 +0800

    最近播放卡片右键菜单

--- NonsPlayer/Components/ViewModels/Home/RecentlyPlayItemCardViewModel.cs
+++ NonsPlayer/Components/ViewModels/Home/RecentlyPlayItemCardViewModel.cs
@@ -5,8 +5,11 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using NonsPlayer.Core.Contracts.Adapters;
 using NonsPlayer.Core.Contracts.Models.Music;
+using NonsPlayer.Core.Nons.Player;
 using NonsPlayer.Core.Services;
 using NonsPlayer.Helpers;
+using NonsPlayer.ViewModels;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace NonsPlayer.Components.ViewModels;
 
@@ -20,5 +23,33 @@ public partial class RecentlyPlayItemCardViewModel
     {
         Music = music;
         Cover = CacheHelper.GetImageBrush(music.Album.CacheAvatarId, music.GetCoverUrl("?param=200x200"));
+    }
+
+    [RelayCommand]
+    public void PlayNext()
+    {
+        PlayQueue.Instance.AddNext(Music);
+    }
+
+    [RelayCommand]
+    public void CheckAlbum()
+    {
+        ServiceHelper.NavigationService.NavigateTo(typeof(AlbumViewModel)?.FullName, Music.Album);
+    }
+
+    [RelayCommand]
+    public void CopyMusicInfo()
+    {
+        var data = new DataPackage();
+        data.SetText($"Share music: {Music.Name} made by {Music.ArtistsName}");
+        Clipboard.SetContent(data);
+    }
+
+    [RelayCommand]
+    public void CopyShareUrl()
+    {
+        var data = new DataPackage();
+        data.SetText(Music.ShareUrl);
+        Clipboard.SetContent(data);
     }
 }
--- NonsPlayer/Components/Views/Home/RecentlyPlayItemCard.xaml.cs
+++ NonsPlayer/Components/Views/Home/RecentlyPlayItemCard.xaml.cs
@@ -1,13 +1,18 @@
 using System.Windows.Forms;
 using Windows.UI.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
 using NonsPlayer.Components.ViewModels;
 using NonsPlayer.Core.Contracts.Adapters;
 using NonsPlayer.Core.Contracts.Models.Music;
 using NonsPlayer.Core.Nons.Player;
 using UserControl = Microsoft.UI.Xaml.Controls.UserControl;
+using Microsoft.UI.Xaml.Controls;
+using NonsPlayer.Helpers;
+using NonsPlayer.ViewModels;
 
 namespace NonsPlayer.Components.Views;
 
@@ -26,10 +31,30 @@ public sealed partial class RecentlyPlayItemCard : UserControl
 
     public IMusic Music
     {
-        set => ViewModel.Init(value);
+        set
+        {
+            for (var i = 0; i < value.Artists.Length; i++)
+            {
+                CheckArtists.Items.Add(new MenuFlyoutItem
+                {
+                    Text = value.Artists[i].Name,
+                    Command = CheckArtistCommand,
+                    Style = App.Current.Resources["CustomMenuFlyoutItem"] as Style,
+                    CommandParameter = value.Artists[i]
+                });
+            }
+
+            ViewModel.Init(value);
+        }
     }
 
-    private void BodyBorder_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+    [RelayCommand]
+    public void CheckArtist(IArtist artist)
+    {
+        ServiceHelper.NavigationService.NavigateTo(typeof(ArtistViewModel)?.FullName, artist);
+    }
+
+    private void BodyBorder_OnTapped(object sender, TappedRoutedEventArgs e)
     {
         PlayQueue.Instance.Play(ViewModel.Music);
     }
