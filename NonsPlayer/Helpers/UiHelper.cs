4e023b5dcc0b4ea999c98a3997514e3c210414e0
Miaoyww <miaomiaoywww@gmail.com>
2024-08-31 23:56:37 +0800

    title bar test

--- NonsPlayer/Helpers/UiHelper.cs
+++ NonsPlayer/Helpers/UiHelper.cs
@@ -2,12 +2,14 @@
 using System.Runtime.InteropServices;
 using Microsoft.UI;
 using Microsoft.UI.Composition;
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using NonsPlayer.Core.Enums;
 using NonsPlayer.Core.Nons.Player;
+using Windows.Graphics;
 using WinRT.Interop;
 using Color = Windows.UI.Color;
 
@@ -130,12 +132,57 @@ internal class TitleBarHelper
     }
 }
 
-public class UiHelper
+public static class DragZoneHelper
 {
-    public static UiHelper Instance { get; } = new();
+    public delegate int SetTitleBarDragRegionDelegate(InputNonClientPointerSource source, SizeInt32 size, double scaleFactor, Func<UIElement, RectInt32?, RectInt32> getScaledRect);
 
-    // 构建歌词修改时间
-    public delegate void LyricChangedHandler(int index);
+    /// <summary>
+    /// Informs the bearer to refresh the drag region.
+    /// will not set<see cref="NonClientRegionKind.LeftBorder"/>, <see cref="NonClientRegionKind.RightBorder"/>, <see cref="NonClientRegionKind.Caption"/>when titleBarHeight less than 0
+    /// </summary>
+    /// <param name="element"></param>
+    /// <param name="window"></param>
+    /// <param name="setTitleBarDragRegion"></param>
+    public static void RaiseSetTitleBarDragRegion(this Window window, SetTitleBarDragRegionDelegate setTitleBarDragRegion)
+    {
+        if (!window.AppWindow.IsVisible)
+            return;
+        // UIElement.RasterizationScale is always 1
+        var source = InputNonClientPointerSource.GetForWindowId(window.AppWindow.Id);
+        var uiElement = window.Content;
+        var xamlRoot = uiElement?.XamlRoot;
 
-    public LyricChangedHandler LyricChanged;
+        if (xamlRoot is null)
+            return;
+
+        var scaleFactor = xamlRoot.RasterizationScale;
+        var size = window.AppWindow.Size;
+        // If the number of regions is 0 or 1, AppWindow will automatically reset to the default region next time, but if it is >=2, it will not and need to be manually cleared
+        source.ClearRegionRects(NonClientRegionKind.Passthrough);
+        var titleBarHeight = setTitleBarDragRegion(source, size, scaleFactor, GetScaledRect);
+        if (titleBarHeight >= 0)
+        {
+            // region under the buttons
+            const int borderThickness = 5;
+            source.SetRegionRects(NonClientRegionKind.LeftBorder, [GetScaledRect(uiElement, new(0, 0, borderThickness, titleBarHeight))]);
+            source.SetRegionRects(NonClientRegionKind.RightBorder, [GetScaledRect(uiElement, new(size.Width, 0, borderThickness, titleBarHeight))]);
+            source.SetRegionRects(NonClientRegionKind.Caption, [GetScaledRect(uiElement, new(0, 0, size.Width, titleBarHeight))]);
+        }
+    }
+
+    private static RectInt32 GetScaledRect(this UIElement uiElement, RectInt32? r = null)
+    {
+        if (r is { } rect)
+        {
+            var scaleFactor = uiElement.XamlRoot.RasterizationScale;
+            return new((int)(rect.X * scaleFactor), (int)(rect.Y * scaleFactor), (int)(rect.Width * scaleFactor),
+                (int)(rect.Height * scaleFactor));
+        }
+        else
+        {
+            var pos = uiElement.TransformToVisual(null).TransformPoint(new(0, 0));
+            rect = new RectInt32((int)pos.X, (int)pos.Y, (int)uiElement.ActualSize.X, (int)uiElement.ActualSize.Y);
+            return GetScaledRect(uiElement, rect);
+        }
+    }
 }
--- NonsPlayer/Views/ShellPage.xaml.cs
+++ NonsPlayer/Views/ShellPage.xaml.cs
@@ -13,10 +13,13 @@ using NonsPlayer.Helpers;
 using NonsPlayer.ViewModels;
 using Microsoft.UI.Windowing;
 using Windows.Graphics;
-using Windows.UI.WindowManagement;
 using Windows.Win32;
 using Windows.Win32.Foundation;
+using AppWindow = Microsoft.UI.Windowing.AppWindow;
+using AppWindowChangedEventArgs = Microsoft.UI.Windowing.AppWindowChangedEventArgs;
 using AppWindowTitleBar = Microsoft.UI.Windowing.AppWindowTitleBar;
+using Microsoft.UI.Input;
+using Windows.Foundation;
 
 namespace NonsPlayer.Views;
 
@@ -35,28 +38,71 @@ public sealed partial class ShellPage : Page
         PlayQueueBarViewModel = App.GetService<PlayQueueBarViewModel>();
         ViewModel.NavigationService.Frame = NavigationFrame;
         WindowHandle = (IntPtr)App.MainWindow.AppWindow.Id.Value;
-
-        // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.
-        // A custom title bar is required for full window theme and Mica support.
-        // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
+        AppTitleBar.SizeChanged += AppTitleBarOnSizeChanged;
+        App.MainWindow.AppWindow.Changed += AppWindowOnChanged;
         App.MainWindow.ExtendsContentIntoTitleBar = true;
-        App.MainWindow.SetTitleBar(AppTitleBar);
         App.MainWindow.Activated += MainWindow_Activated;
-        SetDragRectangles(new RectInt32(0, 0, 100000, (int)(48 * UIScale)));
+
         AppTitleBarText.Text = "AppDisplayName".GetLocalized();
         App.MainWindow.AppWindow.TitleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
         App.MainWindow.AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
         PlayerBar.OnPlayQueueBarOpenHandler += OnOpenPlayQueueButton_Click;
         ExceptionService.Instance.ExceptionThrew += OnExceptionThrew;
+
+        SetRegionsForCustomTitleBar();
     }
 
-    public void SetDragRectangles(params RectInt32[] value)
+    private void AppTitleBarOnSizeChanged(object sender, SizeChangedEventArgs e)
     {
-        if (AppWindowTitleBar.IsCustomizationSupported() && App.MainWindow.AppWindow.TitleBar.ExtendsContentIntoTitleBar == true)
-        {
-            App.MainWindow.AppWindow.TitleBar.SetDragRectangles(value);
-        }
+        SetRegionsForCustomTitleBar();
     }
+
+    private void SetRegionsForCustomTitleBar()
+    {
+        // 获取缩放比例
+        double scaleAdjustment = this.XamlRoot.RasterizationScale;
+
+        // 调整左右填充列的宽度，以适应系统插图
+        SettingsColumn.Width = new GridLength(App.MainWindow.AppWindow.TitleBar.RightInset / scaleAdjustment);
+        TitleBarColumn.Width = new GridLength(App.MainWindow.AppWindow.TitleBar.LeftInset / scaleAdjustment);
+
+        // 获取搜索框的交互区域
+        GeneralTransform transform = Bars.TransformToVisual(null);
+        Rect bounds = transform.TransformBounds(new Rect(0, 0,
+            Bars.ActualWidth,
+            Bars.ActualHeight));
+        Windows.Graphics.RectInt32 SearchBoxRect = GetRect(bounds, scaleAdjustment);
+
+        // 获取账户图片的交互区域
+        transform = AppTitleBar.TransformToVisual(null);
+        bounds = transform.TransformBounds(new Rect(0, 0,
+            AppTitleBar.ActualWidth,
+            AppTitleBar.ActualHeight));
+        RectInt32 PersonPicRect = GetRect(bounds, scaleAdjustment);
+
+        // 定义交互区域数组
+        var rectArray = new [] { SearchBoxRect, PersonPicRect };
+
+        // 设置非客户端区域的交互区域
+        InputNonClientPointerSource nonClientInputSrc =
+            InputNonClientPointerSource.GetForWindowId(App.MainWindow.AppWindow.Id);
+        nonClientInputSrc.SetRegionRects(NonClientRegionKind.Passthrough, rectArray);
+    }
+
+    private Windows.Graphics.RectInt32 GetRect(Rect bounds, double scale)
+    {
+        return new Windows.Graphics.RectInt32(
+            _X: (int)Math.Round(bounds.X * scale),
+            _Y: (int)Math.Round(bounds.Y * scale),
+            _Width: (int)Math.Round(bounds.Width * scale),
+            _Height: (int)Math.Round(bounds.Height * scale)
+        );
+    }
+    private void AppWindowOnChanged(AppWindow sender, AppWindowChangedEventArgs args)
+    {
+        SetRegionsForCustomTitleBar();
+    }
+
 
     public ShellViewModel ViewModel { get; }
 
@@ -83,6 +129,7 @@ public sealed partial class ShellPage : Page
     {
         ViewModel.NavigationService.NavigateTo(typeof(PersonalCenterViewModel).FullName);
     }
+
     public event ShellViewDialogShowEventHandler OnDialogShowCall;
 
     public void OnOpenPlayQueueButton_Click(object? sender, EventArgs e)
@@ -110,11 +157,7 @@ public sealed partial class ShellPage : Page
 
     private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
     {
-        // var resource = args.WindowActivationState == WindowActivationState.Deactivated
-        //     ? "WindowCaptionForegroundDisabled"
-        //     : "WindowCaptionForeground";
-
-        // AppTitleBarText.Foreground = (SolidColorBrush)App.Current.Resources[resource];
+        
     }
 
     private void OnUnloaded(object sender, RoutedEventArgs e)
