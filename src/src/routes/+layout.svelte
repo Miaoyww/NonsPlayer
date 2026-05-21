4b4d6757452e645976867d68d7167e9c3343d453
Miaoyww <miaomiaoywww@gmail.com>
2026-05-21 22:22:40 +0800

    feat: 应用mode-wathcer

--- src/src/routes/+layout.svelte
+++ src/src/routes/+layout.svelte
@@ -1,34 +1,23 @@
 <script lang="ts">
   import "../app.css";
   import "../lib/css/components.css";
-  import { page } from "$app/stores";
+  import { page } from "$app/state";
   import { goto } from "$app/navigation";
-  import { globalSettings } from "$lib/stores/global-settings.store";
+  import { globalSettings } from "$lib/stores/global-settings-store";
   import { onMount } from "svelte";
   import TitleBar from "$lib/components/titlebar.svelte";
   import SettingsDialog from "$lib/components/settings/settings-dialog.svelte";
   import { NONSPLAYER_NAME } from "$lib/const";
   import logo from "$lib/assets/logo.svg";
   import { isTauri } from "@tauri-apps/api/core";
+  import { ModeWatcher } from "mode-watcher";
 
   let { children } = $props();
 
   onMount(() => {
-    const settings = globalSettings;
-    let currentSettings: { welcomeCompleted: boolean } | null = null;
-    const unsub = settings.subscribe((s) => {
-      currentSettings = s;
-    });
-
-    if (
-      currentSettings &&
-      !currentSettings.welcomeCompleted &&
-      !window.location.pathname.startsWith("/welcome")
-    ) {
+    if (!$globalSettings.welcomeCompleted) {
       goto("/welcome");
     }
-
-    unsub();
   });
 </script>
 
@@ -38,11 +27,13 @@
   <link rel="icon" type="image/x-icon" href={logo} />
 </svelte:head>
 
-<TitleBar />
 
+<ModeWatcher />
 <SettingsDialog />
 
-<div class={isTauri() ? 'pt-9' : ''}>
+<TitleBar />
+
+<div class={isTauri() ? "pt-9" : ""}>
   <main>
     {@render children?.()}
   </main>
